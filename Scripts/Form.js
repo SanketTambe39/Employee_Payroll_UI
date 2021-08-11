08941c0578065e527b3b01832fbfc1e981a8ccad
SanketTambe39 <sankettambe39@gmail.com>
2021-08-11 21:37:03 +0530

    ADDED: code to det event listener on salary range to display appropriate value and validation of name and date

--- Scripts/Form.js
+++ Scripts/Form.js
@@ -1,22 +1,18 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function() {
-    output.textContent = salary.value;
-});
-
-const text = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function () {
-    let nameRegex = /^[A-Z]{1}[a-zA-Z]{2,}$/;
-    if (nameRegex.test(text.value)) {
-        textError.textContent = "";
-        document.getElementById("submitButton").disabled = false;
-    }
-    else {
-        textError.textContent = "Name is incorrect";
-        document.getElementById("submitButton").disabled = true;
-    }
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
 });
 
 document.getElementById("submitButton").onclick = function () {
